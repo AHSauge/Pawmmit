5981577a2016ec9b8e8fa0a3460e6edd11feef3c
Alf Henrik Sauge <alf.henrik.sauge@gmail.com>
2026-09-19 12:39:17 +0200

    Fix hidden folders not being followed in the RepositoryWatcher for Linux

--- src/watcher/RepositoryWatcher_qt.cpp
+++ src/watcher/RepositoryWatcher_qt.cpp
@@ -20,7 +20,8 @@
 namespace {
 
 // FIXME: Include hidden and filter .git explicitly?
-const QDir::Filters kFilters = (QDir::Dirs | QDir::NoDotAndDotDot);
+const QDir::Filters kFilters =
+    (QDir::Dirs | QDir::Hidden | QDir::NoDotAndDotDot);
 
 } // namespace
 
